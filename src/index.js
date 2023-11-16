77109f40aeb0c16ac1acbbe43565d8b05433ca46
Chieu Nguyen <chieunguyen0703@hotmail.com>
2023-11-16 13:05:45 +0700

    Update index to use Context

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import AppRouter from './router';
-import { LayoutProvider } from './context/LayoutContext';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import AppRouter from "./router";
+import { LayoutProvider } from "./context/LayoutContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { CssBaseline } from "@mui/material";
+import { CartProvider } from "./users/context/CartContext";
 
-createRoot(document.getElementById('root')).render(
-    <LayoutProvider>
-    <AppRouter />
-  </LayoutProvider>);
+createRoot(document.getElementById("root")).render(
+  <LayoutProvider>
+    <ThemeProvider>
+      <CartProvider>
+        <CssBaseline />
+        <AppRouter />
+      </CartProvider>
+    </ThemeProvider>
+  </LayoutProvider>
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
